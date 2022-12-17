4b107f07dd107567b4377b6fbeafcbea60627c63
arijeetbasu <2105845@reva.edu.in>
2022-12-18 00:28:54 +0530

    init

---
+++ EventBear_Marketplace/hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/3AnIpsDLMrdvuOefuYdJyoG92ux_yJaO",
+      accounts: [ "3d3a4b640b97f09d0b6c057dd14500a7c94ea006c0c7a3ee9dc3de396f5d08e6" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
--- js/main.js
+++ js/main.js
@@ -341,18 +341,18 @@ mailChimp();
         
 
 
-        const tilt = $('.js-tilt').tilt({
-          maxTilt:        20,
-           perspective:    10,   // Transform perspective, the lower the more extreme the tilt gets.
-           easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-           scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
-           speed:          500,    // Speed of the enter/exit transition.
-           transition:     true,   // Set a transition on enter/exit.
-           disableAxis:    null,   // What axis should be disabled. Can be X or Y.
-           reset:          true,   // If the tilt effect has to be reset on exit.
-           glare:          true,  // Enables glare effect
-           maxGlare:       1       // From 0 - 1.
-      });
+      //   const tilt = $('.js-tilt').tilt({
+      //     maxTilt:        20,
+      //      perspective:    10,   // Transform perspective, the lower the more extreme the tilt gets.
+      //      easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+      //      scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
+      //      speed:          500,    // Speed of the enter/exit transition.
+      //      transition:     true,   // Set a transition on enter/exit.
+      //      disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+      //      reset:          true,   // If the tilt effect has to be reset on exit.
+      //      glare:          true,  // Enables glare effect
+      //      maxGlare:       1       // From 0 - 1.
+      // });
 
     
       //  var cursor = document.getElementById('cursor');
